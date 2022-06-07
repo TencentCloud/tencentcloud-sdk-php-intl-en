63014650af6ced0c6b68fbe46a7f87f5be76d03b
tencentcloudapi <tencentcloudapi@tencent.com>
2022-06-08 07:26:20 +0800

    release 3.0.449

--- src/TencentCloud/Scf/V20180416/Models/NamespaceUsage.php
+++ src/TencentCloud/Scf/V20180416/Models/NamespaceUsage.php
@@ -30,10 +30,14 @@ use TencentCloud\Common\AbstractModel;
 Note: This field may return `null`, indicating that no valid values can be obtained.
  * @method void setTotalConcurrencyMem(integer $TotalConcurrencyMem) Set Total memory quota of the namespace
 Note: This field may return `null`, indicating that no valid values can be obtained.
- * @method integer getTotalAllocatedConcurrencyMem() Obtain Memory usage of the namespace
-Note: This field may return `null`, indicating that no valid values can be obtained.
- * @method void setTotalAllocatedConcurrencyMem(integer $TotalAllocatedConcurrencyMem) Set Memory usage of the namespace
-Note: This field may return `null`, indicating that no valid values can be obtained.
+ * @method integer getTotalAllocatedConcurrencyMem() Obtain Concurrency usage of the namespace
+Note: This field may return `null`, indicating that no valid value can be obtained.
+ * @method void setTotalAllocatedConcurrencyMem(integer $TotalAllocatedConcurrencyMem) Set Concurrency usage of the namespace
+Note: This field may return `null`, indicating that no valid value can be obtained.
+ * @method integer getTotalAllocatedProvisionedMem() Obtain Provisioned concurrency usage of the namespace
+Note: This field may return `null`, indicating that no valid value can be obtained.
+ * @method void setTotalAllocatedProvisionedMem(integer $TotalAllocatedProvisionedMem) Set Provisioned concurrency usage of the namespace
+Note: This field may return `null`, indicating that no valid value can be obtained.
  */
 class NamespaceUsage extends AbstractModel
 {
@@ -59,10 +63,16 @@ Note: This field may return `null`, indicating that no valid values can be obtai
     public $TotalConcurrencyMem;
 
     /**
-     * @var integer Memory usage of the namespace
-Note: This field may return `null`, indicating that no valid values can be obtained.
+     * @var integer Concurrency usage of the namespace
+Note: This field may return `null`, indicating that no valid value can be obtained.
      */
     public $TotalAllocatedConcurrencyMem;
+
+    /**
+     * @var integer Provisioned concurrency usage of the namespace
+Note: This field may return `null`, indicating that no valid value can be obtained.
+     */
+    public $TotalAllocatedProvisionedMem;
 
     /**
      * @param array $Functions Function array
@@ -70,8 +80,10 @@ Note: This field may return `null`, indicating that no valid values can be obtai
      * @param integer $FunctionsCount Number of functions in namespace
      * @param integer $TotalConcurrencyMem Total memory quota of the namespace
 Note: This field may return `null`, indicating that no valid values can be obtained.
-     * @param integer $TotalAllocatedConcurrencyMem Memory usage of the namespace
-Note: This field may return `null`, indicating that no valid values can be obtained.
+     * @param integer $TotalAllocatedConcurrencyMem Concurrency usage of the namespace
+Note: This field may return `null`, indicating that no valid value can be obtained.
+     * @param integer $TotalAllocatedProvisionedMem Provisioned concurrency usage of the namespace
+Note: This field may return `null`, indicating that no valid value can be obtained.
      */
     function __construct()
     {
@@ -104,6 +116,10 @@ Note: This field may return `null`, indicating that no valid values can be obtai
 
         if (array_key_exists("TotalAllocatedConcurrencyMem",$param) and $param["TotalAllocatedConcurrencyMem"] !== null) {
             $this->TotalAllocatedConcurrencyMem = $param["TotalAllocatedConcurrencyMem"];
+        }
+
+        if (array_key_exists("TotalAllocatedProvisionedMem",$param) and $param["TotalAllocatedProvisionedMem"] !== null) {
+            $this->TotalAllocatedProvisionedMem = $param["TotalAllocatedProvisionedMem"];
         }
     }
 }
