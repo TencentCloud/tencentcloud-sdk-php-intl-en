260788fe1a33a11bc83c540012c57256520b219a
tencentcloudapi <tencentcloudapi@tencent.com>
2022-12-16 08:28:54 +0800

    release 3.0.594

--- src/TencentCloud/Common/AbstractClient.php
+++ src/TencentCloud/Common/AbstractClient.php
@@ -34,7 +34,7 @@ abstract class AbstractClient
     /**
      * @var string SDK version
      */
-    public static $SDK_VERSION = "SDK_PHP_3.0.593";
+    public static $SDK_VERSION = "SDK_PHP_3.0.594";
 
     /**
      * @var integer http response code 200
