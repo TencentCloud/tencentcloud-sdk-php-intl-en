f67e6fbd4ba694b8b3f0f4867e04fa02b9081eae
tencentcloudapi <tencentcloudapi@tencent.com>
2024-01-16 04:57:40 +0800

    release 3.0.869

--- src/TencentCloud/Common/AbstractClient.php
+++ src/TencentCloud/Common/AbstractClient.php
@@ -34,7 +34,7 @@ abstract class AbstractClient
     /**
      * @var string SDK version
      */
-    public static $SDK_VERSION = "SDK_PHP_3.0.868";
+    public static $SDK_VERSION = "SDK_PHP_3.0.869";
 
     /**
      * @var integer http response code 200
--- src/TencentCloud/Tag/V20180813/Models/TagResourcesRequest.php
+++ src/TencentCloud/Tag/V20180813/Models/TagResourcesRequest.php
@@ -20,25 +20,25 @@ use TencentCloud\Common\AbstractModel;
 /**
  * TagResources request structure.
  *
- * @method array getResourceList() Obtain Cloud resource to be bound, represented in the standard six-segment resource format. For the correct format, see [Resource Description Method](https://intl.cloud.tencent.com/document/product/598/10606?from_cn_redirect=1) and [Tagging-enabled Resources](https://intl.cloud.tencent.com/document/product/651/89122?from_cn_redirect=1).
+ * @method array getResourceList() Obtain Cloud resource to be bound, represented in the standard six-segment resource format. For the correct format, see [Resource Description Method](https://intl.cloud.tencent.com/document/product/598/10606?from_cn_redirect=1).
 Value range of N: 0-9.
- * @method void setResourceList(array $ResourceList) Set Cloud resource to be bound, represented in the standard six-segment resource format. For the correct format, see [Resource Description Method](https://intl.cloud.tencent.com/document/product/598/10606?from_cn_redirect=1) and [Tagging-enabled Resources](https://intl.cloud.tencent.com/document/product/651/89122?from_cn_redirect=1).
+ * @method void setResourceList(array $ResourceList) Set Cloud resource to be bound, represented in the standard six-segment resource format. For the correct format, see [Resource Description Method](https://intl.cloud.tencent.com/document/product/598/10606?from_cn_redirect=1).
 Value range of N: 0-9.
  * @method array getTags() Obtain Tag key and value.
 If multiple tags are specified, all such tags will be created and bound to the specified resources.
 For each resource, each tag key can have only one value. If you try to add an existing tag key, the corresponding tag value will be updated to the new value.
 Non-existent tags will be automatically created.
-Value range of N: 0–9
+Value range of N: 0-9
  * @method void setTags(array $Tags) Set Tag key and value.
 If multiple tags are specified, all such tags will be created and bound to the specified resources.
 For each resource, each tag key can have only one value. If you try to add an existing tag key, the corresponding tag value will be updated to the new value.
 Non-existent tags will be automatically created.
-Value range of N: 0–9
+Value range of N: 0-9
  */
 class TagResourcesRequest extends AbstractModel
 {
     /**
-     * @var array Cloud resource to be bound, represented in the standard six-segment resource format. For the correct format, see [Resource Description Method](https://intl.cloud.tencent.com/document/product/598/10606?from_cn_redirect=1) and [Tagging-enabled Resources](https://intl.cloud.tencent.com/document/product/651/89122?from_cn_redirect=1).
+     * @var array Cloud resource to be bound, represented in the standard six-segment resource format. For the correct format, see [Resource Description Method](https://intl.cloud.tencent.com/document/product/598/10606?from_cn_redirect=1).
 Value range of N: 0-9.
      */
     public $ResourceList;
@@ -48,18 +48,18 @@ Value range of N: 0-9.
 If multiple tags are specified, all such tags will be created and bound to the specified resources.
 For each resource, each tag key can have only one value. If you try to add an existing tag key, the corresponding tag value will be updated to the new value.
 Non-existent tags will be automatically created.
-Value range of N: 0–9
+Value range of N: 0-9
      */
     public $Tags;
 
     /**
-     * @param array $ResourceList Cloud resource to be bound, represented in the standard six-segment resource format. For the correct format, see [Resource Description Method](https://intl.cloud.tencent.com/document/product/598/10606?from_cn_redirect=1) and [Tagging-enabled Resources](https://intl.cloud.tencent.com/document/product/651/89122?from_cn_redirect=1).
+     * @param array $ResourceList Cloud resource to be bound, represented in the standard six-segment resource format. For the correct format, see [Resource Description Method](https://intl.cloud.tencent.com/document/product/598/10606?from_cn_redirect=1).
 Value range of N: 0-9.
      * @param array $Tags Tag key and value.
 If multiple tags are specified, all such tags will be created and bound to the specified resources.
 For each resource, each tag key can have only one value. If you try to add an existing tag key, the corresponding tag value will be updated to the new value.
 Non-existent tags will be automatically created.
-Value range of N: 0–9
+Value range of N: 0-9
      */
     function __construct()
     {
