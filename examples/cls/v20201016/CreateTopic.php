e47e926b47ded3a643564ed0cb4a90606391a7ba
harriszheng <harriszheng@tencent.com>
2025-08-04 20:20:07 +0800

    update notes

--- examples/cls/v20201016/CreateTopic.php
+++ examples/cls/v20201016/CreateTopic.php
@@ -1,34 +1,33 @@
 <?php
 require_once __DIR__.'/../../../vendor/autoload.php';
-// 导入对应产品模块的client
+// Import the client for the corresponding product module
 use TencentCloud\Cls\V20201016\ClsClient;
-// 导入要请求接口对应的Request类
+// Import the Request class for the interface to be called
 use TencentCloud\Cls\V20201016\Models\CreateTopicRequest;
 
 use TencentCloud\Common\Exception\TencentCloudSDKException;
 use TencentCloud\Common\Credential;
-// 导入可选配置类
 use TencentCloud\Common\Profile\ClientProfile;
 use TencentCloud\Common\Profile\HttpProfile;
 
 try {
-    // 实例化一个证书对象，入参需要传入腾讯云账户secretId，secretKey
-    // $cred = new Credential("【secretId】", "【secretKey】");
+    // Instantiate an authentication object. The Tencent Cloud account key pair `secretId` and `secretKey` need to be passed in as the input parameters
+    // $cred = new Credential("secretId", "secretKey");
     $cred = new Credential(getenv("TENCENTCLOUD_SECRET_ID"),
                            getenv("TENCENTCLOUD_SECRET_KEY"));
 
-    // 实例化一个http选项，可选的，没有特殊需求可以跳过
+    // Instantiate an HTTP option (optional; skip if there are no special requirements)
     $httpProfile = new HttpProfile();
-    $httpProfile->setReqMethod("POST");  // post请求(默认为post请求)
-    $httpProfile->setReqTimeout(60);    // 请求超时时间，单位为秒(默认60秒)
-    $httpProfile->setEndpoint("cls.tencentcloudapi.com");  // 指定接入地域域名(默认就近接入)
+    $httpProfile->setReqMethod("POST");  // POST request (default is POST)
+    $httpProfile->setReqTimeout(60);    // Request timeout in seconds (default 60 seconds)
+    $httpProfile->setEndpoint("cls.tencentcloudapi.com");  // Specify the access region domain name (default is nearest access)
 
-    // 实例化一个client选项，可选的，没有特殊需求可以跳过
+    // Instantiate an client option (optional; skip if there are no special requirements)
     $clientProfile = new ClientProfile();
-    $clientProfile->setSignMethod("TC3-HMAC-SHA256");  // 指定签名算法(默认为HmacSHA256)
+    $clientProfile->setSignMethod("TC3-HMAC-SHA256");  // Specify the signature algorithm (default is HmacSHA256)
     $clientProfile->setHttpProfile($httpProfile);
 
-    // 实例化要请求产品(以cls为例)的client对象,clientProfile是可选的
+    // Instantiate the client object for the product to be requested (taking cls as an example). clientProfile is optional
     $client = new ClsClient($cred, "ap-guangzhou", $clientProfile);
 
     $req = new CreateTopicRequest();
@@ -37,7 +36,7 @@ try {
     $req ->setTopicName("cls_php_test_demo");
     $resp = $client->CreateTopic($req);
 
-    // 输出json格式的字符串回包
+    // A string return packet in JSON format is output
     print_r($resp->toJsonString());
 }
 catch(TencentCloudSDKException $e) {
